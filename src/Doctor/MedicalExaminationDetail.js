80d75fc67268ce5f1c65071f6e73c23faede88fb
jacopo <jcpmcc5@gmail.com>
2022-04-28 10:03:40 +0200

    MedicalExam

---
+++ src/Doctor/MedicalExaminationDetail.js
@@ -0,0 +1,140 @@
+import { Col, Table, Form, Button, Container, Row, Carousel } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { useNavigate } from 'react-router-dom';
+import { api } from '../helpers/api/api';
+import { useParams } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
+import moment from 'moment';
+import { path } from '../helpers/Constants';
+import { patient } from '../helpers/api/api';
+
+function MedicalExaminationDetailsInfo() {
+    const location = useLocation();
+    const selectedMedicalExamination = location.state;
+    const selectedPatientId = location.patientId;
+    const [filesArray, setFilesArray] = useState([]);
+
+    const [imgsNames, setImgsNames] = useState([]);
+    const [medicalExamination, setMedicalExamination] = useState(selectedMedicalExamination);
+    const [patientId, setPatientId] = useState(selectedPatientId);
+
+    // const [diagnosticTestId, setDiagnosticTestId] = useState(window.location.pathname.split('/').pop());
+    // const [diagnosticTests, setDiagnosticTests] = useState([]);
+    // const [patientProfile, setPatientProfile] = useState([]);
+
+    useEffect(() => {
+        for (var i = 0; i < selectedMedicalExamination.elencoRefertiVisita.length; i++) {
+            filesArray.push(selectedMedicalExamination.elencoRefertiVisita[i].immagineReferto);
+        }
+        console.log(filesArray);
+    });
+
+    return (
+        <>
+
+            <Row className='col-12 pt-4' >
+                <div className='col-12'>
+                    <h2>Dettaglio visita medica</h2>
+                </div>
+            </Row>
+            &nbsp;&nbsp;
+
+            <Row>
+                <Col>
+                    <ControlledCarouselBloodTests selectedMedicalExamination={selectedMedicalExamination} imgsNames={filesArray} />
+                </Col>
+                <Col>
+                    <div><strong>Visita del:</strong> {medicalExamination.dataVisita}</div>
+                    &nbsp;&nbsp;
+                    <div><h3>Esito della visita</h3></div>
+                    <div>{selectedMedicalExamination.infoAggiuntive}</div>
+                </Col>
+            </Row>
+            <div className='mb-3'>
+                <Button type='submit' >Indietro</Button> <Button type='submit' >Torna a elenco pazienti</Button> <Button type='submit' >Avanti</Button>
+            </div>
+
+        </>
+    );
+}
+
+function ControlledCarouselBloodTests(props) {
+
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
+
+    return (
+        <Carousel activeIndex={index} onSelect={handleSelect} interval={null}>
+            {props.imgsNames?.map((imgName, index) => (
+                <Carousel.Item key={index}>
+                    <img
+                        className="selectedDiagnosticTestImages d-block w-100"
+                        src={path.DIAGNOSTIC_TESTS_IMGS_PATH + imgName.split('\\').pop()}
+                    />
+                    
+                </Carousel.Item>
+            ))}
+            {/* <Carousel.Item >
+                <img
+                    className="selectedDiagnosticTestImages d-block w-50"
+                    src={path.DIAGNOSTIC_TESTS_IMGS_PATH + props.selectedDiagnosticTest.fileName}
+                // alt={img.author}
+                />
+
+            </Carousel.Item> */}
+
+           
+        </Carousel>
+    );
+}
+
+// function BloodTestTable(response) {
+//     return (
+//         <>
+//             <div className='col-6'>
+//                 <Table striped bordered hover size="sm">
+//                     <thead>
+//                         <tr>
+
+//                             <th>Esame</th>
+//                             <th>Valore</th>
+//                         </tr>
+//                     </thead>
+//                     <tbody>
+//                         {
+//                             response.data?.map((drugNameConcentration) => <BloodTestRow key={drugNameConcentration.id} drugNameConcentration={drugNameConcentration} />)
+//                         }
+//                     </tbody>
+//                 </Table>
+//             </div>
+
+
+
+//         </>
+//     );
+// }
+
+// function BloodTestRow(props) {
+//     return <tr><BloodTestRowData drugNameConcentration={props.drugNameConcentration} /></tr>
+// }
+
+// function BloodTestRowData(props) {
+//     return (<>
+//         <td>{props.drugNameConcentration.name === 0 ? "dataEvento" : "dataEvento"}</td>
+//         <td>{props.drugNameConcentration.concentration === 0 ? "disturboEvento" : "disturboEvento"}</td>
+
+//     </>
+//     );
+// }
+
+
+
+
+
+
+export { MedicalExaminationDetailsInfo };
+
---
+++ src/Doctor/MedicalExaminations.js
@@ -0,0 +1,205 @@
+import { Col, Table, Form, Button, Modal, Row } from 'react-bootstrap';
+import { iconDelete, iconEdit } from './icons';
+import { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { useNavigate } from 'react-router-dom';
+import moment from 'moment';
+import { patient } from '../helpers/api/api';
+import { path } from '../helpers/Constants';
+import { Link } from 'react-router-dom';
+import { entitiesLabels, message } from '../helpers/Constants';
+import 'react-notifications/lib/notifications.css';
+import { NotificationContainer, NotificationManager } from 'react-notifications';
+
+function MedicalExaminationsInfo() {
+    const [patientId, setPatientId] = useState(window.location.pathname.split('/').pop());
+    const [medicalExaminations, setMedicalExaminations] = useState([]);
+    const [patientProfile, setPatientProfile] = useState([]);
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    useEffect(() => {
+        const fetchPatient = async () => {
+            await patient.get("Get/", patientId)
+                .then((response) => {
+                    if (response.status === 200) {
+                        setPatientProfile(response.data.dati);
+                    }
+                }).catch((error) => {
+
+                });
+        };
+        fetchPatient();
+    }, []);
+
+    useEffect(() => {
+        const fetchMedicalExaminations = async () => {
+            await patient.get("MedicalExams/", patientId)
+                .then((response) => {
+                    if (response.status === 200) {
+                        setMedicalExaminations(response.data.dati);
+                    }
+                }).catch((error) => {
+
+                });
+        };
+        fetchMedicalExaminations();
+    }, [show]);
+
+    return (
+        <>
+            <Row className='col-12 pt-4' >
+                <div className='col-12'>
+                    <h2>Visite mediche</h2>
+                </div>
+            </Row>
+            &nbsp;&nbsp;
+            <Col className='mb-3'>
+                <MedicalExaminationsTable medicalExaminations={medicalExaminations} patientId={patientId} />
+            </Col>
+            <MedicalExaminationsModal show={show} handleClose={handleClose} patientId={patientId} />
+            <Col className='mb-3'>
+                <Button variant="primary" id="btnAdd" onClick={handleShow}>Aggiungi visita medica <i class="fas fa-plus"></i></Button>
+                {/* <Button onClick={() => editPatient()} >Salva le modifiche</Button> */}
+            </Col>
+        </>
+    );
+}
+
+function MedicalExaminationsTable(props) {
+    return (
+        <>
+            <div className='col-10'>
+                <Table striped bordered hover size="sm">
+                    <thead>
+                        <tr>
+                            <th>Codice visita</th>
+                            <th>Data</th>
+                            <th>Tipo visita</th>
+                            <th>Miniatura</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            props.medicalExaminations?.map((ev) => <MedicalExaminationRow key={ev.id} medicalExamination={ev} patientId={props.patientId} />)
+                        }
+                    </tbody>
+                </Table>
+            </div>
+        </>
+    );
+}
+
+function MedicalExaminationRow(props) {
+    return <tr><MedicalExaminationRowData medicalExamination={props.medicalExamination} patientId={props.patientId} /></tr>
+}
+
+function MedicalExaminationRowData(props) {
+
+    return (<>
+        <td><Link to={`/MedicalExaminationDetails/${props.medicalExamination.id}`} state={props.medicalExamination} patientId={props.patientId} >{props.medicalExamination.id}</Link></td>
+        <td>{props.medicalExamination.dataVisita.split(' ')[0]}</td>
+        <td>{props.medicalExamination.tipoVisita}</td>
+        <td><img src={path.DIAGNOSTIC_TESTS_IMGS_PATH + props.medicalExamination.elencoRefertiVisita[0].immagineReferto} style={{ width: 100, height: 70 }} /></td>
+    </>
+    );
+}
+
+function MedicalExaminationsModal(props) {
+    const [idPatient, setIdPatient] = useState(props.patientId);
+    const [idMedicalExam, setIdidMedicalExam] = useState('0');
+    const [type, setType] = useState();
+    const [data, setData] = useState();
+    const [information, setInformation] = useState();
+    const [file, setFile] = useState([]);
+    const [fileName, setFileName] = useState([]);
+    const [filesArray, setFilesArray] = useState([]);
+
+    function saveMedicalExamination() {
+        // setIdPaziente(parseInt(idPaziente));
+        // setIdidMedicalExam(parseInt(idMedicalExam));
+        const files = new FormData();
+        // files.append("files", file);
+        // files.append("fileName", fileName);
+        filesArray.forEach(file => {
+            files.append("files", file);
+        });
+        console.log(files);
+        patient.postMedicalExamination("Visita/", files, {
+            params:
+                { idPatient, idMedicalExam, type, data, information }, headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    NotificationManager.success(message.PATIENT + message.SuccessUpdate, entitiesLabels.SUCCESS, 3000);
+                }
+            }).catch((error) => {
+                NotificationManager.error(message.ErrorServer, entitiesLabels.ERROR, 3000);
+            });
+        document.getElementById("medicalExaminationForm").reset();
+    };
+
+    const saveFileSelected = (e) => {
+        
+        for (var i = 0; i < e.target.files.length; i++) {
+            filesArray.push(e.target.files.item(i));
+        }
+        console.log(filesArray);
+        // const formData = new FormData();
+        // filesArray.forEach(file => {
+        //     formData.append("arrayOfFilesName", file);
+        // });
+        // console.log(formData);
+        //in case you wan to print the file selected
+        // console.log(formData);
+        // setFile(e.target.files[0]);
+        // setFileName(e.target.files[0].name)
+    };
+
+    return (
+        <>
+            <Modal show={props.show} onHide={props.handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Aggiungi visita medica</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form id="medicalExaminationForm">
+                        <Form.Group controlId="medicalExaminationDate">
+                            <Form.Label>Data referto</Form.Label>
+                            <Form.Control type="date" name="data" placeholder="Data visita" onChange={e => setData(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="medicalExaminationType">
+                            <Form.Label>Tipo visita</Form.Label>
+                            <Form.Control type="text" name="type" placeholder="Tipo visita" onChange={e => setType(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="medicalExaminationInfo">
+                            <Form.Label>Esito visita</Form.Label>
+                            <Form.Control as="textarea" name="information" placeholder="Esito visita" onChange={e => setInformation(e.target.value)} rows={5} />
+                        </Form.Group>
+                        <Form.Group controlId="formFile" className="mb-3">
+                            <Form.Label>Carica file</Form.Label>
+                            <Form.Control type="file" multiple onChange={saveFileSelected} />
+                        </Form.Group>
+
+                    </Form>
+                    < NotificationContainer />
+
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={props.handleClose}>
+                        Chiudi
+                    </Button>
+                    <Button variant="primary" id="btnSave" onClick={saveMedicalExamination}>
+                        Salva crisi
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+
+
+export { MedicalExaminationsInfo };
