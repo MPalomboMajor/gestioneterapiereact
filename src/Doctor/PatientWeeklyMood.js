cc629f12c675fdcd096afb9930b1c80a4603ebb4
jacopo <jcpmcc5@gmail.com>
2022-05-09 16:30:05 +0200

    layout WeeklyMood

--- src/Doctor/PatientWeeklyMood.js
+++ src/Doctor/PatientWeeklyMood.js
@@ -29,11 +29,170 @@ function PatientWeeklyMood(props) {
         setStartDate(sdate);
         setEndDate(edate);
     };
-    
+
 
     return (
         <>
-            <Row>
+            <div>
+                <div className="row align-items-center g-0 g-md-3">
+                    <div className="col-12 col-md-3">
+                        {/* <button className="btn btn-primary mb-3" data-bs-toggle="modal" data-bs-target="#edit">Modifica</button> */}
+                    </div>
+                    <div className="col-12 col-md-5">
+                        <div className="input-group mb-3">
+                            <span className="input-group-text" id="label-inizio">Inizio</span>
+                            <input type="date" className="form-control form-control-sm" id="captiontest" aria-describedby="label-inizio" />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-4">
+                        <div className="input-group mb-3">
+                            <span className="input-group-text" id="label-settimane">settimane</span>
+                            <select className="form-select form-select-sm" id="captiontest" aria-describedby="label-settimane">
+                                <option value={5}>5</option>
+                                <option value={10} selected>10</option>
+                                <option value={15}>15</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div className="row w-100 mt-5 justify-content-center align-items-center g-0 g-md-3">
+                    <div className="col-2 d-none d-md-block">
+                        <div className="label label-primary w-100 mt-4 mb-5">Immagine</div>
+                        <div className="label label-primary w-100 mt-3">Colore</div>
+                    </div>
+                    <div className="col-12 col-md-10">
+                        {/* Slider main container */}
+                        <div className="swiper weekly-mood-carousel">
+                            {/* Additional required wrapper */}
+                            <div className="swiper-wrapper">
+                                {/* Slides */}
+                                <div className="swiper-slide">
+                                    <p className="date">31/01/22 - 06/02/22</p>
+                                    <div className="upper-wrapper">
+                                        <img src="https://picsum.photos/800/600/" />
+                                    </div>
+                                    <div className="row-label" data-label="Immagine" />
+                                    <div className="lower-wrapper">
+                                        <img src="./img/mood/OA_icon-colore-1.svg" />
+                                    </div>
+                                    <div className="row-label" data-label="Colore" />
+                                </div>
+                                <div className="swiper-slide">
+                                    <p className="date">31/01/22 - 06/02/22</p>
+                                    <div className="upper-wrapper">
+                                        <img src="https://picsum.photos/800/601/" />
+                                    </div>
+                                    <div className="row-label" data-label="Immagine" />
+                                    <div className="lower-wrapper">
+                                        <img src="./img/mood/OA_icon-colore-2.svg" />
+                                    </div>
+                                    <div className="row-label" data-label="Colore" />
+                                </div>
+                                <div className="swiper-slide">
+                                    <p className="date">31/01/22 - 06/02/22</p>
+                                    <div className="upper-wrapper">
+                                        <img src="https://picsum.photos/800/602/" />
+                                    </div>
+                                    <div className="row-label" data-label="Immagine" />
+                                    <div className="lower-wrapper">
+                                        <img src="./img/mood/OA_icon-colore-3.svg" />
+                                    </div>
+                                    <div className="row-label" data-label="Colore" />
+                                </div>
+                                <div className="swiper-slide">
+                                    <p className="date">31/01/22 - 06/02/22</p>
+                                    <div className="upper-wrapper">
+                                        <img src="https://picsum.photos/800/603/" />
+                                    </div>
+                                    <div className="row-label" data-label="Immagine" />
+                                    <div className="lower-wrapper">
+                                        <img src="./img/mood/OA_icon-colore-4.svg" />
+                                    </div>
+                                    <div className="row-label" data-label="Colore" />
+                                </div>
+                                <div className="swiper-slide">
+                                    <p className="date">31/01/22 - 06/02/22</p>
+                                    <div className="upper-wrapper">
+                                        <img src="https://picsum.photos/800/604/" />
+                                    </div>
+                                    <div className="row-label" data-label="Immagine" />
+                                    <div className="lower-wrapper">
+                                        <img src="./img/mood/OA_icon-colore-5.svg" />
+                                    </div>
+                                    <div className="row-label" data-label="Colore" />
+                                </div>
+                                <div className="swiper-slide">
+                                    <p className="date">31/01/22 - 06/02/22</p>
+                                    <div className="upper-wrapper">
+                                        <img src="https://picsum.photos/800/605/" />
+                                    </div>
+                                    <div className="row-label" data-label="Immagine" />
+                                    <div className="lower-wrapper">
+                                        <img src="./img/mood/OA_icon-colore-6.svg" />
+                                    </div>
+                                    <div className="row-label" data-label="Colore" />
+                                </div>
+                                <div className="swiper-slide">
+                                    <p className="date">31/01/22 - 06/02/22</p>
+                                    <div className="upper-wrapper">
+                                        <img src="https://picsum.photos/800/606/" />
+                                    </div>
+                                    <div className="row-label" data-label="Immagine" />
+                                    <div className="lower-wrapper">
+                                        <img src="./img/mood/OA_icon-colore-7.svg" />
+                                    </div>
+                                    <div className="row-label" data-label="Colore" />
+                                </div>
+                                <div className="swiper-slide">
+                                    <p className="date">31/01/22 - 06/02/22</p>
+                                    <div className="upper-wrapper">
+                                        <img src="https://picsum.photos/800/607/" />
+                                    </div>
+                                    <div className="row-label" data-label="Immagine" />
+                                    <div className="lower-wrapper">
+                                        <img src="./img/mood/OA_icon-colore-8.svg" />
+                                    </div>
+                                    <div className="row-label" data-label="Colore" />
+                                </div>
+                                <div className="swiper-slide">
+                                    <p className="date">31/01/22 - 06/02/22</p>
+                                    <div className="upper-wrapper">
+                                        <img src="https://picsum.photos/800/608/" />
+                                    </div>
+                                    <div className="row-label" data-label="Immagine" />
+                                    <div className="lower-wrapper">
+                                        <img src="./img/mood/OA_icon-colore-9.svg" />
+                                    </div>
+                                    <div className="row-label" data-label="Colore" />
+                                </div>
+                                <div className="swiper-slide">
+                                    <p className="date">31/01/22 - 06/02/22</p>
+                                    <div className="upper-wrapper">
+                                        <img src="https://picsum.photos/800/609/" />
+                                    </div>
+                                    <div className="row-label" data-label="Immagine" />
+                                    <div className="lower-wrapper">
+                                        <img src="./img/mood/OA_icon-colore-10.svg" />
+                                    </div>
+                                    <div className="row-label" data-label="Colore" />
+                                </div>
+                            </div>
+                            {/* If we need pagination */}
+                            <div className="swiper-pagination" />
+                            {/* If we need navigation buttons */}
+                            <div className="swiper-button-prev" />
+                            <div className="swiper-button-next" />
+                        </div>
+                    </div>
+                </div>
+                <div className="row w-100 mt-5 justify-content-center align-items-start g-0 g-md-3">
+                    <div className="col-12">
+                        <div className="small info">Kumar A, Wang M, Riehm A, Yu E, Smith T, Kaplin A | An Automated Mobile Mood Tracking Technology (Mood 24/7): Validation Study | JMIR Ment Health 2020;7(5):e16237 | https://mental.jmir.org/2020/5/e16237 | doi: 10.2196/16237 | PMID: 32432558</div>
+                    </div>
+                </div>
+            </div>
+
+            {/* <Row>
                 <div className="col-md-4">
                     <Form.Group controlId="startDate">
                         <Form.Label>Inizio</Form.Label>
@@ -52,7 +211,7 @@ function PatientWeeklyMood(props) {
                     <Col>
                         <PatientMoodWeeklyInfo startDate={startDate}  />
                     </Col>
-                </Row>
+                </Row> */}
         </>
 
     );
