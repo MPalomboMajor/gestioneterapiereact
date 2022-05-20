137b0cbfe1989392a332e3ee6d0c728a52aad11b
jacopo <jcpmcc5@gmail.com>
2022-05-20 17:30:00 +0200

    fix icon mood

--- src/icons.js
+++ src/icons.js
@@ -20,7 +20,7 @@ const iconNotFound = <svg xmlns="http://www.w3.org/2000/svg" style={{ marginLeft
     <line x1="12" y1="11" x2="12" y2="14"></line>
 </svg>
 
-const iconNotFoundSmall = <svg xmlns="http://www.w3.org/2000/svg" style={{ marginLeft: '40' }} class="icon icon-tabler icon-tabler-file-alert" width="30" height="30" viewBox="0 0 24 24" stroke-width="2" stroke="#573d6b" fill="#f0b300" stroke-linecap="round" stroke-linejoin="round">
+const iconNotFoundSmall = <svg xmlns="http://www.w3.org/2000/svg" style={{ marginLeft: '40' }} class="icon icon-tabler icon-tabler-file-alert" width="43" height="43" viewBox="0 0 38 24" stroke-width="2" stroke="#573d6b" fill="#f0b300" stroke-linecap="round" stroke-linejoin="round">
     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
     <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
     <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
