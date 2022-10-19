65ac0878a71c99a1ccb21836f4195a081e1feaa0
DESKTOP-MERA6AD\Matteo <m.palombo@majorbit.com>
2022-10-19 16:18:32 +0200

    Search  Medico  by CARE

--- src/CarManager/ListDoctor.js
+++ src/CarManager/ListDoctor.js
@@ -4,13 +4,16 @@ import { user, medico } from '../helpers/api/api';
 import { RowCustom } from "../Doctor/PatientComponent";
 import Pagination from '../helpers/pagination';
 export class ListDoctor extends Component {
-    
+
     constructor(props) {
         super(props);
         this.state = {
             listDoctos: [],
             currentPage: 1,
             itemPerPage: 5,
+            search: '',
+            filteredPatient:[],
+            isFilter:false,
         }
         this.GetListDoctor = this.GetListDoctor.bind(this);
     }
@@ -31,20 +34,54 @@ export class ListDoctor extends Component {
 
 
     }
+    handleChange = (el) => {
+        const inputValue = el.target.value;
+        this.setState({search:
+            inputValue });
+        if (inputValue) {
+            if (this.state.search && this.state.search?.length >= 1) {
+                this.setState({ isFilter: true });
+                let filteredData = this.state.listDoctos.filter(x => String(x.name).includes(inputValue)
+                    || String(x.surName).includes(inputValue)
+                    || String(x.phoneNumber).includes(inputValue)
+                    || String(x.fiscalCode).includes(inputValue)
+                    || String(x.email).includes(inputValue));
+                if (filteredData.length > 0) {
+                    this.setState({
+                        filteredPatient: filteredData,
+                    });
+
+                } else {
+                    this.setState({
+                        filteredPatient: filteredData,
+                    });
+                }
+                this.setState({ currentPage: 1});
+            } else {
+                this.setState({
+                    filteredPatient: this.state.listDoctos,
+                });
+            }
+        } else {
+            this.setState({
+                filteredPatient: this.state.listDoctos,
+            });
+        }
+    };
     setCurrentPage = (n) => {
         this.setState({ currentPage: n });
     }
     setPrevPage = (n) => {
-        this.setState({ currentPage: n-1 });
+        this.setState({ currentPage: n - 1 });
     }
     setNexPage = (n) => {
-        this.setState({ currentPage: n +1 });
+        this.setState({ currentPage: n + 1 });
     }
     render() {
-        
+
         const indexOfLastPatient = this.state.currentPage * this.state.itemPerPage;
         const indexOfFirstPatient = indexOfLastPatient - this.state.itemPerPage;
-        const currentItem = this.state.listDoctos.slice(indexOfFirstPatient, indexOfLastPatient);
+        const currentItem = this.state.isFilter ? this.state.filteredPatient?.slice(indexOfFirstPatient, indexOfLastPatient) : this.state.listDoctos.slice(indexOfFirstPatient, indexOfLastPatient);
         return (
             <Container className="">
                 <Row className='col-12 pt-4' >
@@ -52,33 +89,38 @@ export class ListDoctor extends Component {
                         <h2>Lista Medici </h2>
                     </div>
                 </Row>
+                <Row>
+                    <Form.Group className="col-6 mb-2" >
+                        <Form.Control onChange={this.handleChange}  name="search" alt="medicoDTO" placeholder="Ricerca Assistito" value={this.state.search} />
+                    </Form.Group>
+                </Row>
                 <Row className='col-12 pt-4' >
-                <div className="table-wrapper">
+                    <div className="table-wrapper">
                         <Table className="table custom">
-                                <thead>
-                                    <tr>
-                                        <th>Codice Fiscale</th>
-                                        <th>Cognome</th>
-                                        <th>Nome</th>
-                                        <th>Email</th>
-                                        <th>Centro Medico</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {
-                                        currentItem.map((pa) => <RowCustom colums={["fiscalCode", "surName", "name","email","Centro Medico"]} link={'fiscalCode'} reference={'id'} controller={'DoctorProfile'}  item={pa} /> )
-                                    }
-                                </tbody>
-                            </Table>
-                            </div>
-                            <Pagination
-                                patientsPerPage={5}
-                                totalPatients={this.state.listDoctos.length}
-                                paginate={(pageNumber) => this.setCurrentPage(pageNumber)}
-                                currentPage={this.state.currentPage}
-                                prevPage={(pageNumber) => this.setPrevPage(pageNumber)}
-                                nextPage={(pageNumber) => this.setNexPage(pageNumber)}
-                            />
+                            <thead>
+                                <tr>
+                                    <th>Codice Fiscale</th>
+                                    <th>Cognome</th>
+                                    <th>Nome</th>
+                                    <th>Email</th>
+                                    <th>Centro Medico</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    currentItem.map((pa) => <RowCustom colums={["fiscalCode", "surName", "name", "email", "Centro Medico"]} link={'fiscalCode'} reference={'id'} controller={'DoctorProfile'} item={pa} />)
+                                }
+                            </tbody>
+                        </Table>
+                    </div>
+                    <Pagination
+                        patientsPerPage={5}
+                        totalPatients={this.state.isFilter ?this.state.filteredPatient?.length  :this.state.listDoctos.length}
+                        paginate={(pageNumber) => this.setCurrentPage(pageNumber)}
+                        currentPage={this.state.currentPage}
+                        prevPage={(pageNumber) => this.setPrevPage(pageNumber)}
+                        nextPage={(pageNumber) => this.setNexPage(pageNumber)}
+                    />
                 </Row>
             </Container>
         )
